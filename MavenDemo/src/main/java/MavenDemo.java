7fa3731db55c2ed96d9ae65ed33fb34b5a70c7c0
merveulgur <msulgur92@gmail.com>
2020-05-10 01:49:13 -0700

    Maven Project olusturuldu.

---
+++ MavenDemo/src/main/java/MavenDemo.java
@@ -0,0 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class MavenDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Mavensiz olmaz abi.");
+		
+		System.out.println(StringUtils.isNumeric("234"));
+		System.out.println(StringUtils.isNumeric("merve"));
+
+	}
+
+}
